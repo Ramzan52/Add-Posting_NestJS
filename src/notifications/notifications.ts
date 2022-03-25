0d27c4ab77443598ea1a3529d5ddd1fcd5aa4426
Saad Mehmood <saad.mehmood@aquila360.com>
2022-03-25 18:52:00 +0500

    date fixed

--- src/notifications/notifications.ts
+++ src/notifications/notifications.ts
@@ -6,28 +6,28 @@ export const notifications: NotificationModel[] = [
     id: uuid(),
     picture:
       'https://images.unsplash.com/photo-1640622308069-4352d9b4dcc8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80%22',
-    time: new Date(),
+    time: new Date(new Date().toUTCString()),
     content: 'This is content',
   },
   {
     id: uuid(),
     picture:
       'https://images.unsplash.com/photo-1640622308069-4352d9b4dcc8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80%22',
-    time: new Date(),
+    time: new Date(new Date().toUTCString()),
     content: 'This is content',
   },
   {
     id: uuid(),
     picture:
       'https://images.unsplash.com/photo-1640622308069-4352d9b4dcc8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80%22',
-    time: new Date(),
+    time: new Date(new Date().toUTCString()),
     content: 'This is content',
   },
   {
     id: uuid(),
     picture:
       'https://images.unsplash.com/photo-1640622308069-4352d9b4dcc8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80%22',
-    time: new Date(),
+    time: new Date(new Date().toUTCString()),
     content: 'This is content',
   },
 ];
