d9ab7be02b4962286a947f50b8d0b8b7f54b2242
Saad Mehmood <saad.mehmood@aquila360.com>
2022-03-29 12:11:43 +0500

    and guard

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { NotificationModel } from './models/notification.model';
 import { ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/auth-guards';
 
 @ApiTags('notifications')
+@UseGuards(JwtAuthGuard)
 @Controller('notifications')
 export class NotificationsController {
   constructor(private readonly notificationsService: NotificationsService) {}
